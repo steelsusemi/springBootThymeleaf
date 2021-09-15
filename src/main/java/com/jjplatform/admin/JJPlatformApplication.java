678018f1d8a471f03d52bbeb6fc8303ec2b7b264
steelsusemi <whwncjs95@gmail.com>
2021-09-15 15:36:34 +0900

    최초 commit

---
+++ src/main/java/com/jjplatform/admin/JJPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.jjplatform.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JJPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JJPlatformApplication.class, args);
+    }
+}
